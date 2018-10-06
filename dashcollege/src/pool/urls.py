dcb08bd5db0975d4e855aa64901a7aac74d1ebd8
Harsh Bandhey <raptor419heavy@gmail.com>
2018-10-06 06:00:29 +0530

    progress

--- dashcollege/src/pool/urls.py
+++ dashcollege/src/pool/urls.py
@@ -5,5 +5,10 @@ from django.conf.urls import url
 app_name = 'pool'
 urlpatterns = [
     path('pool/', views.Pool.as_view(), name='pool'),
+    path('pool/cab/', views.Pool.as_view(), name='cab'),
+    path('pool/food/', views.Pool.as_view(), name='food'),
+    path('pool/others/', views.Pool.as_view(), name='others'),
+    path('store/', views.Pool.as_view(), name='store'),
+    path('find/', views.Pool.as_view(), name='store'),
     # path('resource', views.PoolResource.as_view(), name='edit_self'),
 ]
